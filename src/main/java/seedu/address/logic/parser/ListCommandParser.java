9915f1cb98cfe7ce84ec869e40811d399619d98b
Clara Ng <x3Clarawork@gmail.com>
2026-03-10 21:10:09 +0800

    Resolve check style issue

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -3,12 +3,19 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SORT;
 
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new ListCommand object.
+ */
 public class ListCommandParser implements Parser<ListCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListCommand
+     * and returns a ListCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     @Override
     public ListCommand parse(String args) throws ParseException {
 
